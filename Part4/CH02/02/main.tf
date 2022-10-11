a430b626b607848ebbbf7c3ea68a311d25e70c61
i-zro <dek3127@gmail.com>
2022-10-11 17:21:38 +0900

    221011_CH02_02 완료

--- Part4/CH02/02/main.tf
+++ Part4/CH02/02/main.tf
@@ -9,4 +9,18 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "foo" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    "Name" = "This is test vpc"
+  }
+}
+
+output "vpc_foo" {
+  value = aws_vpc.foo
+}
+
+data "aws_vpcs" "bar" {}
+
+output "vpcs" {
+  value = data.aws_vpcs.bar
 }
