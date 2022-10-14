3d0162d2ad58489336b0708c1c2c48a055ee8102
i-zro <dek3127@gmail.com>
2022-10-14 15:32:44 +0900

    221014

--- terraform/9-for/main.tf
+++ terraform/9-for/main.tf
@@ -32,7 +32,7 @@ variable "users" {
 resource "aws_iam_user" "this" {
   for_each = {
     for user in var.users :
-    user.name => user
+    user.name => user  # each의 user.name이 key, user가 value
   }
   
   name = each.key
@@ -43,4 +43,46 @@ resource "aws_iam_user" "this" {
   }
 }
 
+resource "aws_iam_user_group_membership" "this" {
+  for_each = {
+    for user in var.users :
+    user.name => user
+  }
 
+  user = each.key
+  groups = each.value.is_developer ? [aws_iam_group.developer.name, aws_iam_group.employee.name] : [aws_iam_group.employee.name]
+}
+
+locals {
+  developers = [
+    for user in var.users:
+    user  # is_developer가 참일 때만 user 반환
+    if user.is_developer
+  ]
+}
+
+resource "aws_iam_user_policy_attachment" "developer" {  # 사용자 권한 정책 부여 리소스
+  for_each = {
+    for user in local.developers :
+    user.name => user
+  }
+
+  user = each.key
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"  # 관리자 권한
+
+  depends on = [
+    aws_iam_user.this 
+  ]
+}
+
+output "developers" {
+  value = local.developers
+}
+
+output "high_level_users" {
+  value = [
+    for user in var.users :
+    user
+    if user.level > 5
+  ]
+}
